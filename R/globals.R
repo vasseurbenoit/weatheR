d314a3f70733dce85e04f307ed4fc9db81820837
vasseurbenoit <vasseur.benoit62890@protonmail.com>
2025-02-23 18:33:02 -0500

    progress with errors

--- R/globals.R
+++ R/globals.R
@@ -2,5 +2,7 @@ globalVariables(unique(c(
   # p_ombrothermic_diagram:
   "freezing_day", "rain", "T_air_avg", "T_air_max", "T_air_min",
   # c_ombrothermic_diagram:
-  "value", "variable"
+  "value", "variable",
+  # c_weather_anomaly:
+  "difference_precipitation_historical_mean", "difference_temperature_historical_mean", "ratio_precipitation_historical_mean_percentage"
 )))
--- R/t_daily_thermal_unit.R
+++ R/t_daily_thermal_unit.R
@@ -2,86 +2,131 @@
 #'
 #' @description Calculation of the daily thermal unit.
 #' Two methods are available.
-#'    - Method 1: use of T_min, T_max and T_base. Method widely used for
-#'    the calculation of the daily thermal unit of small grain cereals.
-#'    - Method 1: use of T_min, T_max and T_base. Method widely used for
-#'    the calculation of the daily thermal unit of corn.
-#'    - Method 2: use of T_min, T_max, T_base, T_opt and T_max
+#'    - Method A - linear - small grains: use of T_base.
+#'    Method widely used for the calculation of the daily thermal unit of small
+#'    grain cereals.
+#'    - Method B - linear: use of T_air_min, T_air_max and T_base. Method
+#'    widely used for the calculation of the daily thermal unit of corn.
+#'    - Method C - triangle: use of T_air_min, T_air_max, T_base, T_opt and
+#'    T_max.
+#'    - Method D - trapezoid: use of T_air_min, T_air_max, T_base, T_opt_1,
+#'    T_opt_2 and T_max.
 #'
-#' @param T_air_min Minimum air temperature (°C)
-#' @param T_air_max Maximum air temperature (°C)
+#' @param T_air_avg Average air temperature (°C)
 #' @param T_base Base air temperature of a crop (°C)
 #' @param T_opt Optimum air temperature of a crop (°C)
+#' @param T_opt_1 First optimum temperature of a crop (°C)
+#' @param T_opt_2 Second optimum temperature of a crop (°C)
 #' @param T_max Maximum air temperature of a crop (°C)
-#' @param method Selection of method 1 or 2. NOT the third one.
+#' @param method Selection of the calculation method:
+#'  - Method A: Linear - small grains
+#'  - Method B: Linear - corn
+#'  - Method C: Triangle
+#'  - Method D: Trapezoidal
 #'
 #' @return Daily thermal unit (°Cj)
 #' @export
 #'
 #' @examples
-#' # Method 1
-#' t_daily_thermal_unit(T_air_min = 5,
-#'                      T_air_max = 8,
-#'                      T_base = 10,
-#'                      method = 1)
+#' # Method A - linear - small grains
+#' t_daily_thermal_unit(T_air_avg = 15,
+#'                      T_base = 5,
+#'                      method = "A")
 #'
-#' # Method 2
-#' t_daily_thermal_unit(T_air_min = 5,
-#'                      T_air_max = 8,
-#'                      T_base = 10,
-#'                      method = 2)
+#' # Method B - linear - corn
+#' t_daily_thermal_unit(T_air_avg = 15,
+#'                      T_base = 5,
+#'                      method = "B")
 #'
-#' # Method 3
-#' t_daily_thermal_unit(T_air_min = 5,
-#'                      T_air_max = 8,
-#'                      T_base = 10,
+#' # Method C - triangle
+#' t_daily_thermal_unit(T_air_avg = 15,
+#'                      T_base = 5,
 #'                      T_opt = 10,
-#'                      T_max = 20)
-t_daily_thermal_unit <- function(T_air_min,
-                                 T_air_max,
+#'                      T_max = 20,
+#'                      method = "C")
+#'
+#' # Method D - trapezoidal
+#' t_daily_thermal_unit(T_air_avg = 15,
+#'                      T_base = 5,
+#'                      T_opt_1 = 12,
+#'                      T_opt_2 = 18,
+#'                      T_max = 20,
+#'                      method = "D")
+t_daily_thermal_unit <- function(T_air_avg,
                                  T_base,
                                  T_opt = NA,
+                                 T_opt_1 = NA,
+                                 T_opt_2 = NA,
                                  T_max = NA,
-                                 method = 1) {
+                                 method = "A") {
 
-  daily_thermal_unit <-
+  if(method == "A"){ # Linear - small grains
 
-    if(method == 1){
-
-    ## Method 1
-    if((T_air_min + T_air_max)/ 2 < T_base) {
-      T_base
-    } else {
-      (T_air_min + T_air_max)/ 2 - T_base
-    }
-
-  } else if (method == 2){
-
-    ## Method 2
-    T_air_max <- if(T_air_max < T_base){
-      T_base
-    }
-    T_air_min <- if(T_air_min < T_base){
-      T_base
-    }
-
-    (T_air_max + T_air_min)/2 - T_base
+    rate_of_development <- ifelse(T_air_avg < T_base,
+                                  0,
+                                  1)
+    growing_degree_days <- rate_of_development * T_air_avg
 
   } else {
 
-    ## Method 3
-    T_air_avg <- (T_air_min + T_air_max)/ 2
+    if(method == "B"){ # Linear - corn
 
-    if(T_air_avg <= T_base | T_air_avg >= T_max){
-      0
-    } else if(T_air_avg <= T_opt) {
-      (T_air_avg - T_base)/ (T_opt - T_base)
+
     } else {
-      1 - (T_air_avg - T_opt)/ (T_max - T_opt)
+
+      if(method == "C"){ # Triangle
+
+        rate_of_development <-
+          ifelse(
+            T_air_avg <= T_base | T_air_avg >= T_max,
+            0,
+            ifelse(T_air_avg <= T_opt,
+                   (T_air_avg - T_base)/ (T_opt - T_base),
+                   1 - (T_air_avg - T_opt)/ (T_max - T_opt)
+            )
+          )
+
+        growing_degree_days <- rate_of_development * T_air_avg
+
+
+      } else {
+
+        if(method == "D"){ # Trapezoidal
+
+
+          rate_of_development <-
+            ifelse(
+              T_air_avg <= T_base | T_air_avg >= T_max,
+              0,
+              ifelse(
+                T_air_avg <= T_opt_1,
+                (T_air_avg - T_base)/ (T_opt_1 - T_base),
+                ifelse(T_air_avg >= T_opt_2,
+                       1 - (T_air_avg - T_opt_2)/ (T_max - T_opt_2),
+                       1)
+              )
+            )
+
+          growing_degree_days <- rate_of_development * T_air_avg
+
+
+        } else {
+
+          message("Incorrect daily thermal unit method sepcified.")
+
+        }
+
+      }
+
     }
 
   }
 
-  return(daily_thermal_unit)
+
+  output <- list(rate_of_development,
+                 growing_degree_days)
+  names(output) <- c("rate_of_development",
+                     "growing_degree_days")
+  return(output)
 
 }
