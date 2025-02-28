5b6201b4b9d914a627ea85d07d719a489e0f058a
vasseurbenoit <vasseur.benoit62890@protonmail.com>
2025-02-28 10:49:13 -0500

    Updating growing degree-days calculation with linear, triangle and trapezoidal methods.

--- R/t_growing_degree_days.R
+++ R/t_growing_degree_days.R
@@ -1,18 +1,19 @@
-#' t_daily_thermal_unit
+#' Growing degree days calculation
 #'
 #' @description Calculation of the daily thermal unit.
 #' Two methods are available.
-#'    - Method A - linear - small grains: use of T_base.
+#'    - Method A - linear - small grains: use of T_air_avg and T_base.
 #'    Method widely used for the calculation of the daily thermal unit of small
-#'    grain cereals.
+#'    grain cereals and in simulation models.
 #'    - Method B - linear: use of T_air_min, T_air_max and T_base. Method
 #'    widely used for the calculation of the daily thermal unit of corn.
-#'    - Method C - triangle: use of T_air_min, T_air_max, T_base, T_opt and
+#'    - Method C - triangle: use of T_air_avg, T_base, T_opt and T_max.
+#'    - Method D - trapezoid: use of T_air_avg, T_base, T_opt_1, T_opt_2 and
 #'    T_max.
-#'    - Method D - trapezoid: use of T_air_min, T_air_max, T_base, T_opt_1,
-#'    T_opt_2 and T_max.
 #'
-#' @param T_air_avg Average air temperature (°C)
+#' @param T_air_avg Average air temperature (°C). Required for methods A, C, D.
+#' @param T_air_min Minimum air temperature (°C). Required for method B only.
+#' @param T_air_max Maximum air temperature (°C). Required for method B only.
 #' @param T_base Base air temperature of a crop (°C)
 #' @param T_opt Optimum air temperature of a crop (°C)
 #' @param T_opt_1 First optimum temperature of a crop (°C)
@@ -29,36 +30,39 @@
 #'
 #' @examples
 #' # Method A - linear - small grains
-#' t_daily_thermal_unit(T_air_avg = 15,
-#'                      T_base = 5,
-#'                      method = "A")
+#' t_growing_degree_days(T_air_avg = 15,
+#'                       T_base = 5,
+#'                       method = "A")
 #'
 #' # Method B - linear - corn
-#' t_daily_thermal_unit(T_air_avg = 15,
-#'                      T_base = 5,
-#'                      method = "B")
+#' t_growing_degree_days(T_air_min = 15,
+#'                       T_air_max = 25,
+#'                       T_base = 10,
+#'                       method = "B")
 #'
 #' # Method C - triangle
-#' t_daily_thermal_unit(T_air_avg = 15,
-#'                      T_base = 5,
-#'                      T_opt = 10,
-#'                      T_max = 20,
-#'                      method = "C")
+#' t_growing_degree_days(T_air_avg = 15,
+#'                       T_base = 5,
+#'                       T_opt = 10,
+#'                       T_max = 20,
+#'                       method = "C")
 #'
 #' # Method D - trapezoidal
-#' t_daily_thermal_unit(T_air_avg = 15,
-#'                      T_base = 5,
-#'                      T_opt_1 = 12,
-#'                      T_opt_2 = 18,
-#'                      T_max = 20,
-#'                      method = "D")
-t_daily_thermal_unit <- function(T_air_avg,
-                                 T_base,
-                                 T_opt = NA,
-                                 T_opt_1 = NA,
-                                 T_opt_2 = NA,
-                                 T_max = NA,
-                                 method = "A") {
+#' t_growing_degree_days(T_air_avg = 15,
+#'                       T_base = 5,
+#'                       T_opt_1 = 12,
+#'                       T_opt_2 = 18,
+#'                       T_max = 20,
+#'                       method = "D")
+t_growing_degree_days <- function(T_air_avg,
+                                  T_air_min = NA,
+                                  T_air_max = NA,
+                                  T_base,
+                                  T_opt = NA,
+                                  T_opt_1 = NA,
+                                  T_opt_2 = NA,
+                                  T_max = NA,
+                                  method = "A") {
 
   if(method == "A"){ # Linear - small grains
 
@@ -71,6 +75,21 @@ t_daily_thermal_unit <- function(T_air_avg,
 
     if(method == "B"){ # Linear - corn
 
+      T_air_max <- ifelse(T_air_max < T_base,
+                      T_base,
+                      T_air_max)
+
+      T_air_min <- ifelse(T_air_min < T_base,
+                      T_base,
+                      T_air_min)
+
+      T_air_avg <- (T_air_min + T_air_max)/ 2
+
+      rate_of_development <- ifelse(T_air_avg < T_base,
+                                    0,
+                                    1)
+
+      growing_degree_days <- rate_of_development * T_air_avg
 
     } else {
 
